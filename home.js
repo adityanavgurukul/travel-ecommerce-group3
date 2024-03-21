c31e76edcac0b2d135b4020f0b417789df4016f9
Alonecoder81 <ajaykumar810285@gmail.com>
2024-03-21 11:56:59 +0530

    change file name

---
+++ home.js
@@ -0,0 +1,28 @@
+// let menu=document.querySelector("#menu");
+// let nav=document.querySelector("#navbar");
+// window.onscroll=()=>{
+//     menu.classList.remove("fa-times");
+//     navbar.classList.remove("active")
+// }
+// menu.addEventListener(("click",()=>{
+//     menu.classList.toggle("fa-times");
+//     navbar.classList.toggle("active")
+// }));
+
+
+// const id = document.querySelector("#discover");
+// const m = document.querySelector(".spa");
+// let flag=true
+// m.addEventListener("click", () => {
+//     if(flag==true){
+//         id.style.display="grid"
+//         flag=false
+//     }
+//     else{
+//         id.style.display="none"
+//         flag=true
+//     }
+    
+// });
+
+
