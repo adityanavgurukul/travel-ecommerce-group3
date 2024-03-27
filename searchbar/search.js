83c9b2ab813244f8afeb07f9c59c4caa1459d153
SainaSharma62 <shrutisharma993420@gmail.com>
2024-03-27 23:42:50 +0530

    modifie the html,css and js file.

--- searchbar/search.js
+++ searchbar/search.js
@@ -1,102 +1,108 @@
-// const data = [
-//     {
-//        place:"India Gate",city:"Delhi",
-//        place:"Bahai (Lotus) Temple",city: "Delhi",
-//     },
+const product = [
+	{
+		image: "image/bihar2.jpg",
+		rating: 4.5,
+		type: "bihar",
+	},
+    {
+		image: "image/bihar.jpg",
+		rating: 3.8,
+		type: "bihar"
+	},
+    {
+		image: "image/delhi.jpg",
+		rating: 4.0,
+		type: "delhi"
+	},
+    {
+		image: "image/delhi2.jpg",
+		rating: 3.8,
+		type: "delhi"
+	},
+    {
+		image: "image/pune.jpg",
+		rating: 3.6,
+		type: "pune"
+	},
+    {
+		image: "image/pune2.jpg",
+		rating: 3.9,
+		type: "pune"
+	},
 
-//     {
-//        place:"Bodh Gaya",city:"Bihar",
-//        place:"Rajgir Glass Bridge",city: "Bihar",
-//     },
+    {
+		image: "image/banglore2.png",
+		rating: 3.8,
+		type: "banglore"
+	},
+    {
+		image: "image/banglore.png",
+		rating: 3.5,
+		type: "banglore"
+	},
+  {
+		image: "image/up.avif",
+		rating: 3.8,
+		type: "Uttar pradesh"
+	},
+  {
+		image: "image/up2.avif",
+		rating: 3.6,
+		type: "Uttar pradesh"
+	},
+  {
+		image: "image/mp.jpg",
+		rating: 4.2,
+		type: "Madhya pradesh"
+	},
+  {
+		image: "image/mp2.jpg",
+		rating: 4.8,
+		type: "Madhya pradesh"
+	},
 
-//     {
-//         place:"Aga Khan Palace",city:"Pune",
-//         place:"SwamiNarayana-Temple",city: "Pune",
-//     },
-
-//     {
-//         place:"Bhedaghat",city:"Madhya Pradesh",
-//         place:"Pachmarhi",city: "Madhya Pradesh",
-//     },
-
-//     {
-//         place:"Varanasi",city:"Uttar Pradesh",
-//         place:"TajMahal,Agra",city: "Uttar Pradesh",
-//     },
-
-//     {
-//         place:"Bangalore Palace",city:"Banglore,",
-//         place:"Cubbon Park",city:"Banglore,",
-//     }
-// ];
-
-// const searchbtn = document.getElementById("searchButton");
-// const place = document.querySelectorAll(".visit");
-
-
-// const search = () =>{
-//     const searchbox = document.getElementById("locationInput").ariaValueMax.toUpperCase();
-//     const storeitem = document.getElementsByClassName("place");
-//     const place = document.querySelectorAll(".visit");
-//     const location = document.getElementsByClassName("location");
-
-//     for(let i = 0; i < location.length; i++){
-//         let match = place[i].getElementsByClassName('location')[0];
-        
-//         if(match){
-//            let textvalue =  match.textContent || match.innerHTML
-
-//            if(textvalue.toUpperCase().indexOf(searchbox)> -1){
-             
-//              place[i].style.display = "";
-
-//            }else{
-//             place[i].style.display = "none";
-//            }
-//         }
-//     }
-// }
-
-const inputbox = document.getElementById('locationInput');
-const searchButton = document.getElementById('searchbutton');
-
-searchButton.addEventListener('click',function(){
-    const locationInput = inputbox.value.toUpperCase();
-    const visits = document.querySelectorAll('.visit');
-
-    const result = document.getElementById('searchresult');
-    result.innerHTML = '';
-
-    const matchlocation = [];
-
-    visits.forEach(function(visit){
-        const location = visit.querySelector('.location').textContent.toLowerCase();
-        const imgSrc = visit.querySelector('img').src;
-
-        if(location.includes(locationInput)){
-            const locationDiv = document.createElement('div');
-            locationDiv.classList.add('location-item');
+]
 
 
-            const locationName = document.createElement('h2');
-            locationName.textContent = visit.querySelector('label');
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
 
-            const locationImage = document.createElement('img');
-            locationImage.src = imgSrc;
+	const searchData = e.target.value.toLowerCase();
 
-            locationDiv.appendChild(locationName);
-            locationDiv.appendChild(locationImage);
-
-            result.appendChild(locationDiv);
-
-            matchlocation.push(location);
-        }
-    });
-
-      if(matchlocation.length === 0){
-        alert('No locations found matching your search.');
-      }
- 
+	if (searchData === 'bihar') {
+		const shirtData = product.filter(item => item.type.toLowerCase() === 'bihar');
+		displayItem(shirtData);
+	} else if (searchData === 'delhi') {
+		const tshirtData = product.filter(item => item.type.toLowerCase() === 'delhi');
+		displayItem(tshirtData);
+	} else if (searchData === 'pune') {
+		const jeansData = product.filter(item => item.type.toLowerCase() === 'pune');
+		displayItem(jeansData);
+	} else if (searchData === 'banglore') {
+		const lowerData = product.filter(item => item.type.toLowerCase() === 'banglore');
+		displayItem(lowerData);
+	} else if (searchData === 'madhya pradesh') {
+		const lowerData = product.filter(item => item.type.toLowerCase() === 'madhya pradesh');
+		displayItem(lowerData);
+	} else if (searchData === 'uttar pradesh') {
+		const lowerData = product.filter(item => item.type.toLowerCase() === 'uttar pradesh');
+		displayItem(lowerData);
+	}
 });
 
+
+const displayItem = (items) => {
+	document.getElementById('root').innerHTML = items.map((item) => {
+		var { image, rating, price } = item;
+		return (
+			`<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${image}></img>
+                </div> 
+                <div class='bottom'>
+                    <p>Rating: ${rating}</p>
+                </div>
+            </div>`
+		)
+	}).join('')
+};
 
