072b234b01e55d3c351c54fee910f9f6928219f1
Pooja Binjwa <poojasureshbinjwa21@navgurukul.org>
2024-03-27 21:10:30 +0530

    chenge

---
+++ index.js
@@ -0,0 +1,17 @@
+let signupBtn=document.getElementById("signupBtn");
+let LoginBtn= document.getElementById("LoginBtn");
+let title=document.getElementById('title');
+let nameField=document.getElementById("")
+    
+
+    LoginBtn .onclick=function(){
+        nameFi
+        title.innerHTML= "login";
+        signupBtn.classList.add("disable");
+        LoginBtn.classList.remove("disable");
+    }
+    signupBtn .onclick=function(){
+        title.innerHTML= "sign up";
+        signupBtn.classList.remove("disable");
+        signinBtn.classList.add("disable");
+    }
