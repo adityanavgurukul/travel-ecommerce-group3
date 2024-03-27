6d494318fdc5bc5682d4973a4e03562cdebaf2e4
Sehajbhatia-tech <www.sehajpreetkaur009@gmail.com>
2024-03-27 11:30:51 +0530

    modified page

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const quantities = document.querySelectorAll('.quantity');
-
-    quantities.forEach((quantity) => {
-        let count = parseInt(quantity.querySelector('.count').innerText);
-
-        const plusBtn = quantity.querySelector('.plus');
-        const minusBtn = quantity.querySelector('.minus');
-        const countSpan = quantity.querySelector('.count');
-
-        plusBtn.addEventListener('click', () => {
-            count++;
-            countSpan.innerText = count;
-        });
-
-        minusBtn.addEventListener('click', () => {
-            if (count > 1) {
-                count--;
-                countSpan.innerText = count;
-            }
-        });
-    });
-
- 
-
     const checkoutBtn = document.querySelector('.checkOut');
     checkoutBtn.addEventListener('click', () => {
      window.location.href = 'https://pay.google.com'; 
     });
+    
+    const closeBtn = document.querySelector('.close');
+const cart = document.querySelector('.cart');
+
+closeBtn.addEventListener('click', () => {
+    cart.style.animation = 'slideOutRight 0.5s forwards';
 });
+
