62d678a72a1e5f7cd975a34400188f04e9b28922
Sehajbhatia-tech <www.sehajpreetkaur009@gmail.com>
2024-03-27 10:29:02 +0530

    Updated Styling and page

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-document.getElementById("pay-button").addEventListener("click", function() {
-    window.location.href = "https://pay.google.com";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const quantities = document.querySelectorAll('.quantity');
+
+    quantities.forEach((quantity) => {
+        let count = parseInt(quantity.querySelector('.count').innerText);
+
+        const plusBtn = quantity.querySelector('.plus');
+        const minusBtn = quantity.querySelector('.minus');
+        const countSpan = quantity.querySelector('.count');
+
+        plusBtn.addEventListener('click', () => {
+            count++;
+            countSpan.innerText = count;
+        });
+
+        minusBtn.addEventListener('click', () => {
+            if (count > 1) {
+                count--;
+                countSpan.innerText = count;
+            }
+        });
+    });
+
+ 
+
+    const checkoutBtn = document.querySelector('.checkOut');
+    checkoutBtn.addEventListener('click', () => {
+     window.location.href = 'https://pay.google.com'; 
+    });
 });
