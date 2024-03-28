89fdbb8c6e54a816e2c4a4a6ce95a99cfde84f38
SainaSharma62 <shrutisharma993420@gmail.com>
2024-03-28 10:58:34 +0530

    modifie javascript file.

--- searchbar/search.js
+++ searchbar/search.js
@@ -1,64 +1,76 @@
 const product = [
 	{
 		image: "image/bihar2.jpg",
-		rating: 4.5,
-		type: "bihar",
+		name:"Bodhgaya",
+		type: "Bihar",
+		text:"It is famous for being the place where Gautama Buddha is said to have attained enlightenment (Pali: bodhi) under what became known as the Bodhi Tree."
 	},
     {
 		image: "image/bihar.jpg",
-		rating: 3.8,
-		type: "bihar"
+		name:"Glass-Bridge",
+		type: "Bihar",
+		text:"Glass-bridge:A glass bridge is a type of pedestrian bridge that is made with a glass deck or floor, allowing visitors to see through to the ground or water below."
 	},
     {
 		image: "image/delhi.jpg",
-		rating: 4.0,
-		type: "delhi"
+		name:"India-Gate",
+		type: "Delhi",
+		text:"India Gate is a historic monument located in New Delhi, India's capital.It was designed to pay tribute to the soldiers who bravely battled during the First World War."
 	},
     {
 		image: "image/delhi2.jpg",
-		rating: 3.8,
-		type: "delhi"
+		name:"Lotus-temple",
+		type: "Delhi",
+		text:"The Lotus Temple is one of the seven Baha'i House of Worship present around the world. Known for its stunning white petal lotus architecture, iy is one of the most visited establishments in the world."
 	},
     {
 		image: "image/pune.jpg",
-		rating: 3.6,
-		type: "pune"
+		name:"Aga-Khan Palace",
+		type: "Pune",
+		text:"The Aga Khan Palace was built by Sultan Muhammed Shah Aga Khan III in the city of Pune, India. The palace was an act of charity by the spiritual leader of the Nizari Ismaili Muslims."
 	},
     {
 		image: "image/pune2.jpg",
-		rating: 3.9,
-		type: "pune"
+		name:"Swami-Narayana Temple",
+		type: "Pune",
+		text:"Mandir is set upon 140 Carved Pillars. It also has about 109 Beautiful Toran with 10,269 Carved Sculpture and Deities which is Representing the Heritage of Maharashtra and India."
 	},
 
     {
 		image: "image/banglore2.png",
-		rating: 3.8,
-		type: "banglore"
+		name:"Banglore Palace",
+		type: "Banglore",
+		text:"The majestic Bangalore palace is a royal palace known for its architecture with beautiful carvings both inside and outside. It is owned by the Mysore Royal family."
 	},
     {
 		image: "image/banglore.png",
-		rating: 3.5,
-		type: "banglore"
+		name:"Cubbon Park",
+		type: "Banglore",
+		text:"The Cubbon Park has a history of over 100 years. It was established in the year 1870 by Sri John Meade, the then acting Commissioner of Mysore."
 	},
   {
 		image: "image/up.avif",
-		rating: 3.8,
-		type: "Uttar pradesh"
+		name:"Taz-Mahal",
+		type: "Uttar pradesh",
+		text:"The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor,Mumtaz Mahal."
 	},
   {
 		image: "image/up2.avif",
-		rating: 3.6,
-		type: "Uttar pradesh"
-	},
-  {
-		image: "image/mp.jpg",
-		rating: 4.2,
-		type: "Madhya pradesh"
+		name:"Banaras-Ghat",
+		type: "Uttar pradesh",
+		text:"Hindu mythology considers the ghat to be the place of birth of Agni, the God of Fire. A Shiva temple stands here partially submerged in Ganga and is believed to be so heavy that it caused the ghat to collapse into the river."
 	},
   {
 		image: "image/mp2.jpg",
-		rating: 4.8,
-		type: "Madhya pradesh"
+		name:"Pachmarhi",
+		type: "Madhya pradesh",
+		text:"Pachmarhi is the only hill station and is the highest point in Madhya Pradesh. Pachmarhi is also often known as 'Satpura ki Rani' or the Queen of the Satpura Range. Situated at an altitude of 1,067 metres."
+	},
+  {
+		image: "image/mp.jpg",
+		name:"Bhedaghat",
+		type: "Madhya pradesh",
+		text:"Bhedaghat is known for the Dhuandar Falls a huge cascade of water that falls from a height of 98 feet. Boating in Bhedaghat especially during the moonlit night would surely make up for an unforgettable experience. "
 	},
 
 ]
@@ -92,14 +104,16 @@ document.getElementById('searchBar').addEventListener('keyup', (e) => {
 
 const displayItem = (items) => {
 	document.getElementById('root').innerHTML = items.map((item) => {
-		var { image, rating, price } = item;
+		var { image, type, text ,name} = item;
 		return (
 			`<div class='box'>
                 <div class='img-box'>
                     <img class='images' src=${image}></img>
                 </div> 
                 <div class='bottom'>
-                    <p>Rating: ${rating}</p>
+					<p>${name}</p>
+                    <p id = 'type'>${type}</p>
+					<p id = 'text'>${text}</p>
                 </div>
             </div>`
 		)
